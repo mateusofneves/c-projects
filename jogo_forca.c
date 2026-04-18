59286b0aa20eb35da9df2317e25dca85bae607b3
mateusofneves <prmateus.ofn@gmail.com>
2026-04-18 15:52:37 -0300

    colocando a parte do for na funcao jachutou para retornar um valor, por isso tipo int na funcao

--- jogo_forca.c
+++ jogo_forca.c
@@ -7,11 +7,25 @@ void abertura() {
     printf("*********************\n\n");
 }
 
-void chuta(char chutes[26], int tentativas) {
+void chuta(char chutes[26], int* tentativas) {
     char chute;
     scanf(" %c", &chute);
 
-    chutes[tentativas] = chute;
+    chutes[(*tentativas)] = chute;
+    (*tentativas)++;
+}
+
+ int jachutou(char letra, char chutes[26], int* tentativas) {
+    
+    int achou = 0;
+
+    for(int j = 0; j < *tentativas; j++) {
+        if(chutes[j] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+    return achou;
 }
 
 int main() {
@@ -31,14 +45,8 @@ int main() {
     do {
 
         for(int i = 0; i < strlen(palavrasecreta); i++) {
-            int achou = 0;
 
-            for(int j = 0; j < tentativas; j++) {
-                if(chutes[j] == palavrasecreta[i]) {
-                    achou = 1;
-                    break;
-                }
-            }
+            int achou = jachutou(palavrasecreta[i], chutes, &tentativas);
 
             if(achou) {
                 printf("%c ", palavrasecreta[i]);
@@ -49,8 +57,7 @@ int main() {
         printf("\n");
 
         //chamando a função de chute para ler o chute do usuário e armazenar no array de chutes
-        chuta(chutes, tentativas);
-        tentativas++;
+        chuta(chutes, &tentativas);
 
     } while (!acertou && !enforcou);
 
