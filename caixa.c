99250cef0214f5ec80750d68f8c3dfdc63808a61
mateusofneves <prmateus.ofn@gmail.com>
2026-05-15 11:43:12 -0300

    otimizando o código

--- caixa.c
+++ caixa.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 void exibirMenu() {
-
     printf("\n========================\n");
     printf("     CAIXA ELETRONICO     \n");
-    printf("  ========================\n");
+    printf("========================\n");
     printf("1 - Consultar Saldo\n");
     printf("2 - Realizar Saque\n");
     printf("3 - Realizar Deposito\n");
@@ -15,81 +13,97 @@ void exibirMenu() {
     printf("Escolha uma opcao: ");
 }
 
-void consultarSaldo(float saldo) {
+void pausar() {
+    printf("\nPressione Enter para continuar...");
+    getchar();
+    getchar();
+}
 
+void consultarSaldo(float saldo) {
     printf("\n--- SALDO ATUAL ---\n");
     printf("R$ %.2f\n", saldo);
     printf("-------------------\n");
-    system("pause"); 
+    pausar();
 }
 
 float realizarDeposito(float saldo) {
-
     float valor;
 
     printf("Digite o valor do deposito: ");
-    scanf(" %f", &valor);
-    
-    if (valor > 0) 
-    {
+    if (scanf("%f", &valor) != 1) {
+        printf("Entrada invalida!\n");
+        while (getchar() != '\n');
+        return saldo;
+    }
+
+    if (valor > 0) {
         saldo += valor;
-        printf("Deposito realizado!\n");
+        printf("Deposito realizado com sucesso!\n");
     } else {
         printf("Valor invalido!\n");
     }
 
+    pausar();
     return saldo;
 }
 
 float realizarSaque(float saldo) {
-
     float valor;
 
-    printf("Valor do saque: ");
-    scanf(" %f", &valor);
+    printf("Digite o valor do saque: ");
+    if (scanf("%f", &valor) != 1) {
+        printf("Entrada invalida!\n");
+        while (getchar() != '\n');
+        return saldo;
+    }
 
-    if (valor <= saldo && valor > 0) {
+    if (valor > 0 && valor <= saldo) {
         saldo -= valor;
-        printf("Saque realizado!\n");
+        printf("Saque realizado com sucesso!\n");
     } else {
         printf("Saldo insuficiente ou valor invalido!\n");
     }
 
+    pausar();
     return saldo;
 }
 
 int main() {
-
     int opcao;
+    float saldo = 0.0f;
 
-    float saldo;
-
-    do
-    {
+    do {
         exibirMenu();
-        scanf(" %d", &opcao);
 
-        switch (opcao)
-        {
+        if (scanf("%d", &opcao) != 1) {
+            printf("Entrada invalida!\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        switch (opcao) {
             case 0:
-                printf("\nObrigado por usar nosso programa.");
+                printf("\nObrigado por usar nosso programa!\n");
                 break;
 
             case 1:
                 consultarSaldo(saldo);
                 break;
-        
+
             case 2:
                 saldo = realizarSaque(saldo);
                 break;
+
             case 3:
                 saldo = realizarDeposito(saldo);
-        
+                break;
+
             default:
-                printf("Opcao invalida.");
+                printf("Opcao invalida!\n");
+                pausar();
         }
 
     } while (opcao != 0);
-    
+
     return 0;
 }
